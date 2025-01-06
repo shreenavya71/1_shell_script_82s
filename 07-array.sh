c231126fbcf7914a6546ae7d68742db3cb3e773c
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2025-01-06 22:02:59 +0000

    second script variables added

---
+++ 07-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("puspa" "rrr" "devara")
+
+echo "first movie: ${MOVIES[0]}"
+echo "second movie: ${MOVIES[1]}"
+echo "third movie: ${MOVIES[2]}"
+
+echo "all movies are: ${MOVIES[@]}"
