cdbae4b74749b59a9f9ad92c415ecac4a179087c
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2025-01-06 13:09:16 +0000

    second script variables added

--- 04-variables-with-args.sh
+++ 04-variables-with-args.sh
@@ -7,5 +7,5 @@ PERSON2=$2 # no spaces before and after equal (=)
 
 echo "$PERSON1:: hi $PERSON2"
 echo "$PERSON2:: hi $PERSON1"
-echo "$PERSON1:: how are you doing robert"
+echo "$PERSON1:: how are you doing"
 echo "$PERSON2:: i am fine, how are you? "
