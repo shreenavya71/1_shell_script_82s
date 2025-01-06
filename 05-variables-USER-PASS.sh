f5b13759200b83558c324bb4bb4be9a3ff174696
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2025-01-06 13:18:47 +0000

    second script variables added

---
+++ 05-variables-USER-PASS.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "please enter username::"
+read -s USERNAME
+echo "username entered is $USERNAME"
+
+echo "please enter password::"
+read -s PASSWORD
+echo "password entered is $PASSWORD"
