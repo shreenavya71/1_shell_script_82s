3f39384e31cdd56ee933abab1f8263168e48e261
shreenavya71 <85473764+shreenavya71@users.noreply.github.com>
2025-01-16 14:11:41 +0000

    shell script

--- 10_install_script.sh
+++ 10_install_script.sh
@@ -5,7 +5,27 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "error:: Please run this script as rootuser or with sudo."
+    exit 1   # other than 0
 fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "error:: Failed to install MySQL."
+    exit 1
+else
+    echo "MySQL installation successful."
+fi
+
+
 
 dnf install git -y
 
+if [ $? -ne 0 ]
+then
+    echo "error:: Failed to install GIT."
+    exit 1
+else
+    echo "GIT installation successful."
+fi
